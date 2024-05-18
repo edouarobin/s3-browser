a0c77673c34a58cde196b9b3b21c4dd15d982428
Edouard R <erobin@intramuros.org>
2024-05-18 23:40:19 +0200

    missing changed

--- src/services/aws/S3.ts
+++ src/services/aws/S3.ts
@@ -4,13 +4,15 @@ import {
   ListObjectsV2Command,
   ListObjectsV2CommandOutput,
   S3Client,
+  GetObjectCommand,
 } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { IBucketList } from '../../model/s3';
 
 const S3_ENDPOINT_URL = process.env.S3_ENDPOINT_URL || 'https://s3.amazonaws.com';
 
 const s3 = new S3Client({
-  region: 'ap-southeast-1',
+  region: process.env.S3_REGION,
   endpoint: process.env.S3_ENDPOINT_URL,
   forcePathStyle: true,
 });
@@ -24,33 +26,58 @@ export const listBuckets = async (): Promise<any> => {
   }
 };
 
+const calculateSignedURL = async (data: any) => {
+  const getObjectCommand = new GetObjectCommand({ Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME, Key: data?.Key });
+  const client = new S3Client({
+    region: process.env.S3_REGION,
+    endpoint: process.env.S3_ENDPOINT_URL,
+    forcePathStyle: true,
+  })
+  const signedURL = await getSignedUrl(client, getObjectCommand, { expiresIn: 3600 });
+  return signedURL
+}
+
 export const listObjects = async (prefix = ''): Promise<IBucketList[]> => {
   try {
-    const response: ListObjectsV2CommandOutput = await s3.send(
-      new ListObjectsV2Command({
-        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-        Prefix: prefix,
-        Delimiter: '/',
-      })
-    );
+    const listObjectsCommand = new ListObjectsV2Command({
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+      Prefix: prefix,
+      Delimiter: '/',
+    })
+
+    const response: ListObjectsV2CommandOutput = await s3.send(listObjectsCommand);
+    // console.log(response)
+
     const keyOrPrefixList = [];
     keyOrPrefixList.push(
       ...(response.CommonPrefixes
         ? response.CommonPrefixes.map((data) => ({
-            keyOrPrefix: data.Prefix || '',
-            baseUrl: S3_ENDPOINT_URL,
-          }))
+          keyOrPrefix: data.Prefix || '',
+          baseUrl: S3_ENDPOINT_URL,
+          signedURL: '',
+        }))
         : [])
     );
+
+    
     keyOrPrefixList.push(
       ...(response.Contents
-        ? response.Contents?.map((data) => ({
-            keyOrPrefix: data.Key || '',
-            baseUrl: S3_ENDPOINT_URL,
-          }))
+        ? response.Contents?.map(async (data: any) => {
+          const signedURL = await calculateSignedURL(data);
+          return (
+            {
+              keyOrPrefix: data.Key || '',
+              baseUrl: S3_ENDPOINT_URL,
+              signedURL: signedURL,
+            }
+          )
+        })
         : [])
     );
-    return keyOrPrefixList;
+
+    const content = await Promise.all(keyOrPrefixList);
+    
+    return content;
   } catch (err) {
     console.log(err);
     return [];
