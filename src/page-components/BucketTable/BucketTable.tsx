d5416e0733f57f537d3bc05e5e7eaf2782470fa0
Edouard R <erobin@intramuros.org>
2024-06-02 16:47:22 +0200

    video player

--- src/page-components/BucketTable/BucketTable.tsx
+++ src/page-components/BucketTable/BucketTable.tsx
@@ -2,6 +2,7 @@ import { Table } from 'antd';
 import Column from 'antd/lib/table/Column';
 import Link from 'next/link';
 import { FunctionComponent } from 'react';
+import Player from '../VideoPlayer/Player';
 import { IBucketList } from '../../model/s3';
 
 type BucketTableProps = {
@@ -27,38 +28,66 @@ const generateBreadcrumbs = (pathList: string[]) => {
 
 export const BucketTable: FunctionComponent<BucketTableProps> = ({ keyOrPrefixList, prefix }) => {
   return (
-    <Table dataSource={keyOrPrefixList} pagination={false} rowKey="keyOrPrefix">
-      <Column
-        title={
-          prefix
-            ? generateBreadcrumbs(prefix.split('/')).map((obj) => (
-                <>
-                  <Link key={obj.link} href={obj.link}>
-                    {obj.crumb}
-                  </Link>
-                  {' / '}
-                </>
-              ))
-            : '/'
-        }
-        dataIndex="keyOrPrefix"
-        key="keyOrPrefix"
-        render={(keyOrPrefix, row: IBucketList) => {
-          if (keyOrPrefix.charAt(keyOrPrefix.length - 1) === '/') {
-            keyOrPrefix = keyOrPrefix.replace(/\/\s*$/, '');
-            return <Link href={`/${keyOrPrefix}`}>{`${keyOrPrefix.split('/').pop()}/`}</Link>;
+    <>
+      {prefix
+        ? generateBreadcrumbs(prefix.split('/')).map((obj) => (
+          <>
+            <Link key={obj.link} href={obj.link}>
+              {obj.crumb}
+            </Link>
+            {' / '}
+          </>
+        ))
+        : '/'}
+      <div style={{ "display": "grid", "gridTemplateColumns": "auto auto" }}>
+        {keyOrPrefixList?.map((item: any) => {
+          if (item.keyOrPrefix.charAt(item.keyOrPrefix.length - 1) === '/') {
+            let keyOrPrefix = item.keyOrPrefix.replace(/\/\s*$/, '');
+            return <div key={item.keyOrPrefix} style={{ "marginTop": 20 }}><Link href={`/${keyOrPrefix}`}>{`${keyOrPrefix.split('/').pop()}/`}</Link></div>;
           }
-          return (
-            <a
-              target="_blank"
-              href={row.signedURL || `${row.baseUrl}/${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}/${keyOrPrefix}`}
-              rel="noopener noreferrer"
-            >
-              {keyOrPrefix.split('/').pop()}
-            </a>
-          );
-        }}
-      />
-    </Table>
+          return <div key={item.signedURL}>
+            <Player firstURL={item.signedURL} muted playbackRate={0.25} width={"100%"} height={800} controls={false} loop />
+          </div>
+        })}
+      </div>
+    </>
+    // <Table dataSource={keyOrPrefixList} pagination={false} rowKey="keyOrPrefix">
+    //   <Column
+    //     title={
+    //       prefix
+    //         ? generateBreadcrumbs(prefix.split('/')).map((obj) => (
+    //           <>
+    //             <Link key={obj.link} href={obj.link}>
+    //               {obj.crumb}
+    //             </Link>
+    //             {' / '}
+    //           </>
+    //         ))
+    //         : '/'
+    //     }
+    //     dataIndex="keyOrPrefix"
+    //     key="keyOrPrefix"
+    //     render={(keyOrPrefix, row: IBucketList) => {
+    //       if (keyOrPrefix.charAt(keyOrPrefix.length - 1) === '/') {
+    //         keyOrPrefix = keyOrPrefix.replace(/\/\s*$/, '');
+    //         return <Link href={`/${keyOrPrefix}`}>{`${keyOrPrefix.split('/').pop()}/`}</Link>;
+    //       }
+    //       return (
+
+    //         <div key={row.signedURL}>
+    //           <Player firstURL={row.signedURL} muted playbackRate={0.25} width={"auto"} height={400} controls loop/>
+    //           {/* <a
+    //             target="_blank"
+    //             href={row.signedURL || `${row.baseUrl}/${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}/${keyOrPrefix}`}
+    //             rel="noopener noreferrer"
+    //           >
+    //             {keyOrPrefix.split('/').pop()}
+    //           </a> */}
+    //         </div>
+
+    //       );
+    //     }}
+    //   />
+    // </Table>
   );
 };
--- src/pages/[[...path]].tsx
+++ src/pages/[[...path]].tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useRouter } from 'next/dist/client/router';
 import { useEffect, useState } from 'react';
-import useSWR from 'swr';
+import useSWRImmutable from 'swr/immutable';
 import { BaseLayout } from '../page-components/BaseLayout/BaseLayout';
 import { BucketTable } from '../page-components/BucketTable/BucketTable';
 
@@ -22,17 +22,20 @@ const getPrefix = (path: string | string[] | undefined): string => {
 export default function Home() {
   const router = useRouter();
   const [prefix, setPrefix] = useState(getPrefix(router.query.path));
+
   useEffect(() => {
     setPrefix(getPrefix(router.query.path));
   }, [router]);
-  const { data: keyOrPrefixList, error } = useSWR(
+
+  const { data: keyOrPrefixList, error } = useSWRImmutable(
     `/api/list-objects${prefix ? `?prefix=${prefix}/` : ''}`,
     getFetcher
   );
+
   return (
     <BaseLayout>
-      <div style={{ padding: '16px 50px', minHeight: 'calc(100vh - 64px)' }}>
-        <div style={{ fontSize: '18px', fontWeight: 'bold', padding: '0 0 16px 0' }}>
+      <div style={{ padding: '0px 10px', minHeight: 'calc(100vh - 64px)' }}>
+        <div style={{ fontSize: '18px', fontWeight: 'bold', padding: '0 0 0px 0' }}>
           Bucket: {process.env.NEXT_PUBLIC_S3_BUCKET_NAME}
         </div>
         <BucketTable prefix={prefix} keyOrPrefixList={keyOrPrefixList} />
